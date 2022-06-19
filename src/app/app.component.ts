48cde0862cca7f57d6d1979ca79dc08f096f3236
Diego Cantelli <diegocantelli19@gmail.com>
2022-06-19 11:11:56 -0300

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'AluraPic';
+
+  title = 'alurapic';
+  description = 'Leão';
+  url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg';
 }
