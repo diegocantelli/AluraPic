2c51c3be3097176c2a01e08a75031a78ce7a40c9
Diego Cantelli <diegocantelli19@gmail.com>
2022-06-20 18:49:11 -0300

    Criando o component photo-list e photo-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Photo } from './photos/photo/photo';
 import { PhotoService } from './photos/photo/photo.service';
+import { PhotosModule } from './photos/photos.module';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,4 @@ import { PhotoService } from './photos/photo/photo.service';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit{
-
-  photos: Photo[] = [];
-
-  constructor(private photoService: PhotoService){}
-
-  //Ocorre apos a instanciacao da classe e de receber as inboud properties
-  ngOnInit(): void{
-    this.photoService.listFromUser('flavio')
-      .subscribe(photos => this.photos = photos);
-  }
-
-}
+export class AppComponent {}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,10 +1,20 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
 import { PhotoComponent } from "./photo/photo.component";
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-    declarations: [ PhotoComponent ], //Os componentes que este módulo usa/precisa
-    imports: [HttpClientModule],
-    exports: [ PhotoComponent ] //Torna o componente acessível para quem importar este módulo
+    declarations: [
+      PhotoComponent,
+      PhotoListComponent,
+      PhotoFormComponent
+    ], //Os componentes que este módulo usa/precisa
+    imports: [
+      HttpClientModule,
+      CommonModule //modulo que importa as diretivas ngIf, ngFor e etc
+    ]
 })
 export class PhotosModule {}
