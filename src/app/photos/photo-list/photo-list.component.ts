579e9891e7e99e07164ff5ecaa664756eb0a0936
Diego Cantelli <diegocantelli19@gmail.com>
2022-06-24 18:30:48 -0300

    Implementando o subject e ou debounceTime para realizar o filtro das fotos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { debounceTime, Subject } from 'rxjs';
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
@@ -12,12 +13,17 @@ export class PhotoListComponent implements OnInit{
 
   photos: Photo[] = [];
   filter: any  = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute){}
 
   //Ocorre apos a instanciacao da classe e de receber as inboud properties
   ngOnInit(): void{
     this.photos = this.activatedRoute.snapshot.data['photos'];
+
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
   }
 
 }
