94eb96a074a4f940c2964bc4b06dea9d5fe025ce
Diego Cantelli <diegocantelli19@gmail.com>
2022-06-25 11:28:01 -0300

    implementando o botao load more e entendendo o modo de deteccao de propriedades do angular

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -14,16 +14,32 @@ export class PhotoListComponent implements OnInit, OnDestroy{
   photos: Photo[] = [];
   filter: any  = '';
   debounce: Subject<string> = new Subject<string>();
+  currentPage: number = 1;
+  userName: string = '';
+  hasMore: boolean = true;
 
-  constructor(private activatedRoute: ActivatedRoute){}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService){}
 
   //Ocorre apos a instanciacao da classe e de receber as inboud properties
   ngOnInit(): void{
+    this.userName = this.activatedRoute.snapshot.params['userName'];
     this.photos = this.activatedRoute.snapshot.data['photos'];
 
     this.debounce
     .pipe(debounceTime(300))
     .subscribe(filter => this.filter = filter);
+  }
+
+  load(){
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        //é necessario atribuir uma nova instancia a photos para que o mecanismo de deteccao do angular saiba que essa propriedade mudou
+        this.photos = this.photos.concat(photos);
+        if(!photos.length) this.hasMore = false
+      })
   }
 
   ngOnDestroy(): void {
