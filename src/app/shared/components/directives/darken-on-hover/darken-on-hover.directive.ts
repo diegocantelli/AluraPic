b44aab6e0ec6cdf61a79731b48cce5df8394f8cb
Diego Cantelli <diegocantelli19@gmail.com>
2022-06-26 11:17:44 -0300

    Utilizando Renderer para manipular o css dos elementos

--- src/app/shared/components/directives/darken-on-hover/darken-on-hover.directive.ts
+++ src/app/shared/components/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -1,21 +1,25 @@
-import { Directive, ElementRef, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from "@angular/core";
 
 @Directive({
   selector: '[apDarkenOnHover]'
 })
 export class DarkenOnHoverDirective{
 
-  constructor(private elementRef: ElementRef){
+  @Input() brightness = '70%'
 
-  }
+  constructor(
+    private elementRef: ElementRef,
+    private render: Renderer2){}
 
   @HostListener('mouseover') //irá escutar o evento no qual a diretiva apDarkenOnHover estiver colocada
   darkenOn(){
+    this.render.setStyle(this.elementRef.nativeElement, 'filter', `brightness(${this.brightness})`);
     console.log('darken on');
   }
 
   @HostListener('mouseleave')
   darkenOff(){
+    this.render.setStyle(this.elementRef.nativeElement, 'filter', 'brightness(100%)');
     console.log('darken off');
   }
 }
