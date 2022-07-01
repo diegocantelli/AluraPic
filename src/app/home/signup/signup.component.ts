89683f964cc68f6e4aa477895fc9587011160ee0
Diego Cantelli <diegocantelli19@gmail.com>
2022-07-01 19:32:25 -0300

    Criando o component signup e criando o formulario e adicionando validacoes

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,6 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signup.component.html'
 })
-export class SignUpComponent { }
+export class SignUpComponent implements OnInit {
+
+  signupForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      userName: ['',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]
+      ],
+      fullName: ['',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(30)
+        ]
+      ],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(14)
+        ]
+      ]
+    })
+  }
+}
