7cc2be34de76a6c812bb641c4acaf61ae94bb2eb
diegocantelli <diegocantelli19@gmail.com>
2022-08-27 14:41:03 -0300

    Implementando a diretiva no local correto

--- src/app/shared/directives/immediate-click.module.ts
+++ src/app/shared/directives/immediate-click.module.ts
@@ -1,13 +1,19 @@
-import { Directive, ElementRef } from "@angular/core";
+import { Directive, ElementRef, OnInit } from "@angular/core";
+import { PlatformDetectorService } from "src/app/core/platform/platform-detector.service";
 
 @Directive({
   selector: '[immediateClick]'
 })
 
-export class ImmediateClickDirective{
+export class ImmediateClickDirective implements OnInit {
+
   constructor(
-    private element: ElementRef<any>
-  ){
-    element.nativeElement.click();
+    private element: ElementRef<any>,
+    private platformDetector: PlatformDetectorService
+  ){}
+
+  ngOnInit(): void {
+    this.platformDetector.isPlatformBrowser() &&
+      this.element.nativeElement.click();
   }
 }
