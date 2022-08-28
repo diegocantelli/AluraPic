2be85ab6d4df2f2878694e44dd9a85d48d9f6fb7
diegocantelli <diegocantelli19@gmail.com>
2022-08-28 15:31:02 -0300

    Criando o componente Photo-Comments para lidar com os comentarios das fotos

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { PhotoModule } from "../photo/photo.module";
+import { PhotoCommentsComponent } from "./photo-comments/photo-comments.component";
 import { PhotoDetailsComponent } from "./photo-details.component";
 
 @NgModule({
-  declarations: [ PhotoDetailsComponent ],
-  exports: [ PhotoDetailsComponent ],
+  declarations: [
+    PhotoDetailsComponent,
+    PhotoCommentsComponent
+  ],
+  exports: [
+    PhotoDetailsComponent,
+    PhotoCommentsComponent
+  ],
   imports: [
     CommonModule,
     PhotoModule
