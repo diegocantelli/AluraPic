165f6db7ead4c87d929fcb75f1879e1b1ff143d5
diegocantelli <diegocantelli19@gmail.com>
2022-09-03 10:58:36 -0300

    Ajustando o componente de enviar comentarios da foto

--- src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from "@angular/forms";
 import { Observable } from "rxjs";
 import { PhotoComment } from "../../photo/photo-comment";
 import { PhotoService } from "../../photo/photo.service";
@@ -11,13 +12,28 @@ import { PhotoService } from "../../photo/photo.service";
 export class PhotoCommentsComponent implements OnInit {
 
   @Input() photoId!: number;
+  commentForm!: FormGroup;
 
   comments$ = new Observable<PhotoComment[]>();
 
-  constructor(private photoService: PhotoService){}
+  constructor(
+    private photoService: PhotoService,
+    private formBuilder: FormBuilder){}
 
   ngOnInit(): void {
     this.comments$ = this.photoService.getComments(this.photoId);
+
+    this.commentForm = this.formBuilder.group({
+      comment: ['', Validators.maxLength(300)]
+    })
+  }
+
+  checkFormFieldErrosValidation(errorsArray: ValidationErrors | null | undefined, error: string): boolean{
+    if(!errorsArray) return false;
+
+    if(!errorsArray[error]) return false;
+
+    return true;
   }
 
 }
