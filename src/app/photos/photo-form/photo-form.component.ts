9a346ef07a2e4cb4938db2b7c117de1cd8d2a6f1
diegocantelli <diegocantelli19@gmail.com>
2022-10-01 14:35:53 -0300

    Exibindo a msg de que a foto foi enviado com sucesso

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -17,7 +18,8 @@ export class PhotoFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private photoService: PhotoService,
-    private router: Router) { }
+    private router: Router,
+    private alertService: AlertService) { }
 
   ngOnInit(): void {
     this.photoForm = this.formBuilder.group({
@@ -36,7 +38,10 @@ export class PhotoFormComponent implements OnInit {
 
     this.photoService
       .upload(description, allowComments, this.file)
-      .subscribe(() => this.router.navigate(['']));
+      .subscribe(() => {
+        this.alertService.success('Upload complete');
+        this.router.navigate([''])
+      });
 
 
   }
