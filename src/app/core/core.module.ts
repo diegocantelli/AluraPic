c94b796fd4b8630a986892ae1300381e0002a8de
diegocantelli <diegocantelli19@gmail.com>
2022-10-15 12:35:42 -0300

    Exibindo itens no menu apenas se o usuario estiver logado

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,8 @@ import { RouterModule } from "@angular/router";
 import { AlertModule } from "../shared/components/alert/alert.module";
 import { LoadingModule } from "../shared/components/loading/loading.module";
 import { MenuModule } from "../shared/components/menu/menu.module";
+import { ShowIfLoggedDirective } from "../shared/directives/show-if-logged/show-if-logged.directive";
+import { ShowIfLoggedModule } from "../shared/directives/show-if-logged/show-if-logged.module";
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
 import { RequestInterceptor } from "./request.interceptor";
@@ -19,7 +21,8 @@ import { RequestInterceptor } from "./request.interceptor";
     AlertModule,
     RouterModule, //permite usar a diretiva routerLink dentre outras
     LoadingModule,
-    MenuModule
+    MenuModule,
+    ShowIfLoggedModule
   ],
   exports: [
     HeaderComponent,
